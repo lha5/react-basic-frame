367531c3ad6a2e57e5bed774f611be0e628658bb
LEE HYEAHN <hyeahnlee5@gmail.com>
2020-10-23 10:20:52 +0900

    fixed component pages

---
+++ src/components/views/ProductSelectionAndDevelopment/KeywordSearch/KeywordSearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function KeywordSearchPage() {
+    return (
+        <div>
+            키워드 검색
+        </div>
+    )
+}
+
+export default KeywordSearchPage
---
+++ src/components/views/ProductSelectionAndDevelopment/SmartKeyword/SmartKeywordPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SmartKeywordPage() {
+    return (
+        <div>
+            스마트 키워드
+        </div>
+    )
+}
+
+export default SmartKeywordPage
